fefba7fc14af13f649671851cc145fa7e317cd4f
fukiame <fukiame@proton.me>
2025-03-01 09:55:36 +0700

    blossom: init: tune lmkd for 2G
    - slightly bump partial / complete stall timeout
      to delay the kill and allow for some recovery attempt
    - clamp thrash limit and thrash limit decay
      to actually kill on unreasonably high mem pressure
    
    Test: open Milthm, play サイクルの欠片 with Nightmare Ripples, observe no crash
    Signed-off-by: fukiame <fukiame@proton.me>

--- init/init_blossom.cpp
+++ init/init_blossom.cpp
@@ -107,16 +107,26 @@ void vendor_load_properties()
         heapmaxfree = "8m";
         // from lmkd defaults for low ram devices
         // tuned a bit "higher end"
-    property_override("ro.lmk.psi_partial_stall_ms", partialstall);
-    property_override("ro.lmk.psi_complete_stall_ms", completestall);
-    property_override("ro.lmk.thrashing_limit", thrashlim);
-    property_override("ro.lmk.thrashing_limit_decay", thrashlimdec);
-    property_override("ro.lmk.swap_free_low_percentage", swapfreelow);
-    property_override("ro.lmk.upgrade_pressure", upressure);
+        partialstall = "135";
+        completestall = "540";
+        thrashlim = "50";
+        thrashlimdec = "40";
+        swapfreelow = "15";
+        upressure = "70";
+        property_override("ro.config.art_lowmem", "true");
+    }
+
     property_override("dalvik.vm.heapstartsize", heapstartsize);
     property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
     property_override("dalvik.vm.heapsize", heapsize);
     property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
+
+    property_override("ro.lmk.psi_partial_stall_ms", partialstall);
+    property_override("ro.lmk.psi_complete_stall_ms", completestall);
+    property_override("ro.lmk.thrashing_limit", thrashlim);
+    property_override("ro.lmk.thrashing_limit_decay", thrashlimdec);
+    property_override("ro.lmk.swap_free_low_percentage", swapfreelow);
+    property_override("ro.lmk.upgrade_pressure", upressure);
 }
