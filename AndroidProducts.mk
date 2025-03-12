18734f9c8b097ee59b9bf79764592b7618b5bbeb
Jayed khan <tanvirgaming859@gmail.com>
2025-03-13 04:43:20 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_blossom.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_blossom-user \
-    lineage_blossom-userdebug \
-    lineage_blossom-eng
+    alpha_blossom-user \
+    alpha_blossom-userdebug \
+    alpha_blossom-eng
