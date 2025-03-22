0ef737dcc01acaa9767ece8fa2276676d282ee87
Jayed khan <tanvirgaming859@gmail.com>
2025-03-23 04:48:30 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_blossom.mk
+    $(LOCAL_DIR)/blaze_blossom.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_blossom-user \
-    lineage_blossom-userdebug \
-    lineage_blossom-eng
+    blaze_blossom-user \
+    blaze_blossom-userdebug \
+    blazee_blossom-eng
