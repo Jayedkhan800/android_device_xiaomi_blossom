2fc55002328803f4ea95b35b0e84fae9980f67ed
Jayed khan <tanvirgaming859@gmail.com>
2025-07-02 00:50:18 +0600

    Adopt for Derp from los

--- derp_blossom.mk
+++ derp_blossom.mk
@@ -12,16 +12,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/blossom/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common derp stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 scr_resolution := 720
 TARGET_SUPPORTS_BLUR := true
 TARGET_ENABLE_BLUR := true
+DERP_BUILDTYPE := Unofficial
 
-PRODUCT_NAME := lineage_blossom
+PRODUCT_NAME := derp_blossom
 PRODUCT_DEVICE := blossom
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+     DERP_MAINTAINER=Jayed \
+     DERP_BUILD=$(DERP_BUILDTYPE)
