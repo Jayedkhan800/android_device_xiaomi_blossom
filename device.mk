84ee3929fae98a93f47dfe4d9a472eedc8d2cd6e
LuK1337 <priv.luk@gmail.com>
2025-11-16 10:56:33 +0100

    blossom: Opt out of speaker_layout_channel_mask field
    Required for ABI compatibility with audio HAL compiled before 15 QPR2.
    
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/9426
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>
    Signed-off-by: fukiame <fukiame@proton.me>

--- device.mk
+++ device.mk
@@ -156,7 +156,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
 #Speaker_layout_channel_mask field
-$(call soong_config_set_bool,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
+$(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_field,true)
 
 PRODUCT_PACKAGES += \
     libstagefright_softomx_plugin.vendor
