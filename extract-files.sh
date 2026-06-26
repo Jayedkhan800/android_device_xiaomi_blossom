4ef8f78c8884e5b29c81dd785990be5e708d59de
Sarthak Roy <sarthakroy2002@gmail.com>
2026-06-27 02:50:17 +0600

    blossom: Patch  to address SetTaskProfiles symbol

--- extract-files.sh
+++ extract-files.sh
@@ -69,6 +69,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libsink.so" "libsink-mtk.so" "${2}"
             ;;
+        vendor/lib64/libutils-v30.so)
+            [ "$2" = "" ] && return 0
+            grep -q "libprocessgroup_shim.so" "${2}" || "${PATCHELF}" --add-needed "libprocessgroup_shim.so" "${2}"
+            ;;
         lib/libsource.so)
             grep -q libshim_ui.so "$2" || "$PATCHELF" --add-needed libshim_ui.so "$2"
             ;;
